b55ecf54dfcb727a6a8832f3c758277b19b0cd61
Hirushan N.N.M.H <it21129476@my.sliit.lk>
2022-10-29 03:06:15 +0530

    room order ui add

--- app/src/main/java/com/example/royalorbitlast/ReserveRooms.java
+++ app/src/main/java/com/example/royalorbitlast/ReserveRooms.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RoomORderConfirm extends AppCompatActivity {
+public class ReserveRooms extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_room_order_confirm);
+        setContentView(R.layout.activity_reserve_rooms);
     }
 }
